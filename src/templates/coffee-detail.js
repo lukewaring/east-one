dd9145d502533872afee9df545c3b8616a4588e5
lukewaring <lukewaring@gmail.com>
2020-05-12 12:16:38 -0400

    Add buy button to coffee-detail template; tweak template styles

--- src/templates/coffee-detail.js
+++ src/templates/coffee-detail.js
@@ -8,6 +8,7 @@ export const query = graphql`
     allDatoCmsProduct(filter: { slug: { eq: $slug } }) {
       edges {
         node {
+          id
           country
           cropYear
           elevation
@@ -37,24 +38,49 @@ export default function(context) {
         {data.name} — {data.country}
       </h1>
       <div className="CoffeeDetailContainer">
-        <div></div>
-        <Img fixed={data.image.fixed} />
-        <div>
-          <h2>${data.price}</h2>
+        <Img className="CoffeeDetailImg" fixed={data.image.fixed} />
+        <div className="CoffeeDetailDetails">
+          <h2 className="CoffeeDetailPrice">${data.price}</h2>
           <br></br>
           <p1>Region: {data.region}</p1>
           <br></br>
+          <br></br>
           <p1>Producer: {data.producer}</p1>
+          <br></br>
           <br></br>
           <p1>Elevation: {data.elevation}</p1>
           <br></br>
+          <br></br>
           <p1>Process: {data.processing}</p1>
+          <br></br>
           <br></br>
           <p1>Varietal: {data.varietal}</p1>
           <br></br>
+          <br></br>
           <p1>Crop Year: {data.cropYear}</p1>
           <br></br>
+          <br></br>
           <p1>{data.notes}</p1>
+          <br></br>
+          <br></br>
+          <button
+            className="CoffeeDetail__buy Product snipcart-add-item"
+            data-item-id={data.id}
+            data-item-name={data.name}
+            data-item-price={data.price}
+            data-item-country={data.country}
+            data-item-region={data.region}
+            data-item-producer={data.producer}
+            data-item-elevation={data.elevation}
+            data-item-processing={data.processing}
+            data-item-varietal={data.varietal}
+            data-item-cropYear={data.cropyear}
+            data-item-notes={data.notes}
+            data-item-image={data.image.url}
+            data-item-url={`/`}
+          >
+            Add to cart
+          </button>
         </div>
       </div>
     </Layout>
